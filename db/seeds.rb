3f07a14d791a8704a134a87d78e2edfc73284325
Colin Bertin <colinbertin12@gmail.com>
2022-02-18 13:55:45 +0900

    seeding moree movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,21 @@
 require 'open-uri'
 Movie.destroy_all
+puts "Destroying the movies..."
 # List.destroy_all
 
-# the Le Wagon copy of the API
-url = 'https://api.themoviedb.org/3/movie/popular?api_key=c60e8b2d743239f99317ff6a0d177935&language=en-US&page=1'
-response = JSON.parse(URI.open(url).read)
 
-response['results'].each do |movie_hash|
-  Movie.create!(
-    poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
-    title: movie_hash['title'],
-    overview: movie_hash['overview'],
-    rating: movie_hash['vote_average']
-  )
+# the Le Wagon copy of the API
+(1..5).each do |count|
+  url = "https://api.themoviedb.org/3/movie/popular?api_key=c60e8b2d743239f99317ff6a0d177935&language=en-US&page=#{count}"
+  el = JSON.parse(URI.open(url).read)
+  el['results'].each do |movie_hash|
+    Movie.create(
+      poster_url: "https://image.tmdb.org/t/p/w500#{movie_hash['poster_path']}",
+      title: movie_hash['title'],
+      overview: movie_hash['overview'],
+      rating: movie_hash['vote_average']
+    )
+  end
 end
+
+puts "#{Movie.count} have been seeded!"
